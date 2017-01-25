7218fd3dc31f5b1627037bfc45f2a2d0ae9ec4b5
Neel Smith <dnsmith.neel@gmail.com>
2017-01-25 13:24:55 -0500

    Load local files in service.

--- src/main/scala/scs/CiteServices.scala
+++ src/main/scala/scs/CiteServices.scala
@@ -2,10 +2,16 @@ package edu.holycross.shot.scs
 
 import io.finch.route._
 import com.twitter.finagle.Httpx
+import com.twitter.util.Await
+import java.io.File
 
 object CiteServices extends App {
-  Httpx.serve(":8080", (Get / "scs" /> "CITE  services will live here").toService)
 
-  println("Press <enter> to exit.")
-  Console.in.read.toChar
+  import com.twitter.io.{Reader, Buf}
+  val config = new java.io.File(".").getCanonicalPath
+  val ctsCatalog = new java.io.File(config,"catalog.txt")
+  val msg = "CITE  services will live here, and use CTS cataloged in " + ctsCatalog.toString()
+
+  Await.ready(Httpx.serve(":8080", (Get / "scs" /> msg).toService))
+
 }
